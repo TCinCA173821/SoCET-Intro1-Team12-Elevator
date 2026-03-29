585aa24c389e93b5cc894c31e4a0821795c96221
Anirudh Vummadising <137233762+ANIRUDH1423@users.noreply.github.com>
2026-03-29 15:40:34 -0400

    Create elevator_top.sv
    add fsm later

---
+++ elevator_top.sv
@@ -0,0 +1,52 @@
+module elevator_top (
+	input logic clk,
+	input logic rst_n,
+
+	input logic [4:0] inside_btn,  //floors 
+	input logic btn_open,
+	input logic btn_close,
+
+	input logic [3:0] call_btn_up,
+	input logic [3:0] call_btn_down,
+
+//outputs
+
+	output logic [6:0] seg, //a 7 segments display (easiest to display #s)
+	output logic led_up, //shows dir. up
+	output logic led_down, //shows dir. down
+	output logic led_door //door open indicator
+);
+
+// internal wires, no data being processed here
+
+// curr_floor and next_floor are 3 wires because they can represent 0-7 in binary instead of doing 5 seperate wires (one per floor)
+
+	logic [2:0] curr_floor;
+	logic [2:0] next_floor;
+	logic door_open; // door status
+	logic door_open_req;
+	logic door_close_req;
+	logic moving; //if elevator is in motion we need to control certain outputs
+	logic dir_up; //no need for dir_down, this tells us if motion is downwards too
+
+// floor controller
+
+	floor_controller u_ctrl (
+		.clk (clk),
+		.rst_n (rst_n),
+		.inside_btn (inside_btn),
+		.call_btn_up (call_btn_up),
+		.call_btn_down (call_btn_down),
+		.btn_open (btn_open),
+		.btn_close (btn_close),
+		.curr_floor (curr_floor),
+		.door_open (door_open),
+		.moving (moving),
+
+		//outputting 
+		.next_floor (next_floor),
+		.door_open_req (door_open_req),
+		.door_close_req (door_close_req),
+	);
+		
+
